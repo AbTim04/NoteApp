234bb53990357e5c2b3b164713e456a7841ee999
AbTim04 <abdeev04t@gmail.com>
2022-05-04 00:15:47 +0700

    Создание проекта юнит-тестов и одного теста для проверки работоспособности

---
+++ NoteApp/NoteApp.UnitTests/NoteTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using NoteApp.Library;
+
+namespace NoteApp.UnitTests
+{
+    [TestFixture]
+    public class NoteTests
+    {
+        /// <summary>
+        /// Экземпляр класса <see cref="Note"/> для проведения тестов
+        /// </summary>
+        private Note _note;
+
+        /// <summary>
+        /// Метод создания пустой заметки
+        /// </summary>
+        public Note Note_Init()
+        {
+            _note = new Note();
+            return _note;
+        }
+
+        [Test(Description = "Позитивный тест геттера и сеттера Name")]
+        public void Title_CorrectValue_ReturnsSameValue()
+        {
+            //Setup
+            var note = Note_Init();
+            var expected = "Test title for note";
+
+            //Act
+            note.Name = expected;
+            var actual = note.Name;
+
+            //Assert
+            Assert.AreEqual(actual, expected,
+                "Геттер или сеттер Name возвращает неправильный текст");
+        }
+    }
+}
---
+++ NoteApp/NoteApp.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NoteApp.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("*")]
+[assembly: AssemblyProduct("NoteApp.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © * 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b7c8e2ce-d76c-4ad9-8409-676f3f5749da")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
